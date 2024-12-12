3b77945ad6cd3757a09552fd120b5cb7526c09e7
moeezali <moeezali2375@gmail.com>
2024-12-12 14:12:07 +0500

    Bug Fix

--- backend/routes/protectRoutes.js
+++ backend/routes/protectRoutes.js
@@ -3,6 +3,6 @@ const protect = require("../middlewares/authMiddleware");
 const userVerify = require("../middlewares/verifyMiddleware");
 const { checkLogin } = require("../controllers/protectControllers");
 
-router.get("/", protect, userVerify,adminMiddleware, checkLogin);
+router.get("/", protect, userVerify, checkLogin);
 
 module.exports = router;
